3be64ed87739bb4b725f74f4c2dd0e0d0e217073
Sean Treppa <saturnofsands@gmail.com>
2016-04-05 23:11:57 -0400

    MAx_health Increased
    max_health was changed to 600.

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -5,7 +5,7 @@ data:extend({
     icon = "__Molten Salt Thorium Reactor__/graphics/reactor/icon-reactor.png",
     flags = {"placeable-neutral", "player-creation"},
     minable = {hardness = 0.2, mining_time = 0.5, result = "reactor"},
-    max_health = 100,
+    max_health = 600,
     corpse = "big-remnants",
     collision_box = {{-6.4, -2.4}, {2.0, 2.4}},
     selection_box = {{-6.5, -2.5}, {2.1, 2.5}},
